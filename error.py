7f0767944734a6885a779cae0ccbfaac590451c3
Coricos <meryll.dindin@student.ecp.fr>
2017-03-02 13:29:48 +0100

    Implemented logs handler

---
+++ error.py
@@ -0,0 +1,19 @@
+# @author : DINDIN Meryll
+
+# Imports
+
+import os
+import datetime
+
+# Define class handling errors as new log purpose
+
+class Error:
+
+	def __init__(self):
+		self.path = '../logs.txt'
+		self.time = datetime.datetime.now()
+
+	def log(self, message):
+		file = open(self.path, 'a')
+		file.write('[{}] : {}'.format(str(self.time), message+'\n'))
+		file.close()
