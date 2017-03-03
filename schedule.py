f07620bffcb82c94a2bb388bd802f3bd2f6e9032
Coricos <meryll.dindin@student.ecp.fr>
2017-03-03 11:44:40 +0100

    Added scheduled job for qai requests

--- schedule.py
+++ schedule.py
@@ -14,6 +14,9 @@ api_eed = RequestEedomus()
 schedule.every().day.at('08:00').do(api_eed.get_data())
 schedule.every().day.at('20:00').do(api_eed.get_data())
 
+api_qai = RequestQAI()
+schedule.every().day.at('23:00').do(api_qai.get_date())
+
 # Launch
 
 while True :
