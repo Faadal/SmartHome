ac6b05bd9e5aa82a32b4ed2279d4279e840eda04
Coricos <meryll.dindin@student.ecp.fr>
2017-03-03 10:32:57 +0100

    First commit of my scheduler

---
+++ schedule.py
@@ -0,0 +1,21 @@
+# @author : DINDIN Meryll
+
+# Imports
+
+import schedule
+import datetime
+import time
+
+from api import *
+
+# Get rid of the usual crontab
+
+api_eed = RequestEedomus()
+schedule.every().day.at('08:00').do(api_eed.get_data())
+schedule.every().day.at('20:00').do(api_eed.get_data())
+
+# Launch
+
+while True :
+	schedule.run_pending()
+	time.sleep(1)
