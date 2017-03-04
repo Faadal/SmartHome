72dcf267aa76a33c71ac3c8c7022d7ccd538139a
Coricos <meryll.dindin@student.ecp.fr>
2017-03-04 23:36:39 +0100

    Solved conflict with non connected sensors

--- processing.py
+++ processing.py
@@ -57,89 +57,95 @@ class Sampler:
 		except :
 			err.log('Could not read data for sensor {}'.format(sensor))
 
-		# Save the amount of acquisitions realized by the considered sensor
-		try :
-			pwd = open('../Acquisition/Acq_{}_{}.txt'.format(sensor[:1], sensor[1:]), 'a')
-			pwd.write('Le {} :\n'.format(self.dte.strftime('%d/%m/%Y')))
-			pwd.write('{} acquisitions\n'.format(len(tem)))
-			pwd.close()
-			msg.log('Acquisitions have been saved for sensor {}'.format(sensor))
-		except :
-			err.log('Impossible to update the acquisition process')
+		if len(tem) == 0 :
+			err.log('No data for sensor {}'.format(sensor))
 
-		def time_slot(sensor):
+		else :
+			# Save the amount of acquisitions realized by the considered sensor
+			try :
+				pwd = open('../Acquisition/Acq_{}_{}.txt'.format(sensor[:1], sensor[1:]), 'a')
+				pwd.write('Le {} :\n'.format(self.dte.strftime('%d/%m/%Y')))
+				pwd.write('{} acquisitions\n'.format(len(tem)))
+				pwd.close()
+				msg.log('Acquisitions have been saved for sensor {}'.format(sensor))
+			except :
+				err.log('Impossible to update the acquisition process')
 
-			if sensor[:1] in ['T', 'H', 'L'] :
-				return [float(int(10*(0.0+k*0.1)))/10.0 for k in range(241)]
-			else :
-				return [float(int(10*(0.0+k*0.01)))/10.0 for k in range(2401)]
+			def time_slot(sensor):
 
-		tab = np.zeros(len(time_slot(sensor)))
-
-		# End of processing for the gathered time serie
-		try :
-			tsl = [ele[1] for ele in tem]
-			val = [ele[0] for ele in tem]
-			for ind, ele in enumerate(tsl) :
-				if int(10*ele) >= 0 and int(10*ele) < len(time_slot(sensor)) :
-					tab[int(10*ele)] = val[ind]
-		except :
-			err.log('Failure for algorithmic extraction and sorting for sensor {}'.format(sensor))
-
-		def remplissage(values) :
-
-			# Determine le premier indice non nul de la liste    
-			def first(liste) :
-				i = 0
-				l = liste
-				if l[0] != 0 : return(i)
-				else : 
-					while (l[0] == 0 and len(l) > 1) :
-						i += 1
-						l = l[1:]
-					return(i)
-
-			# Determine le dernier indice non nul de la liste    
-			def last(liste) :
-				j = 0
-				l = liste
-				if l[-1] != 0 : return(j)
+				if sensor[:1] in ['T', 'H', 'L'] :
+					return [float(int(10*(0.0+k*0.1)))/10.0 for k in range(241)]
 				else :
-					while (l[-1] == 0 and len(l)>1) :
-						j += 1
-						l = l[:-1]
-					return(j)
+					return [float(int(10*(0.0+k*0.01)))/10.0 for k in range(2401)]
 
-			i, j = first(values), last(values)
+			tab = np.zeros(len(time_slot(sensor)))
 
-			if i >= 1 :
-				for k in range(i) : 
-					values[k] = values[i]
+			# End of processing for the gathered time serie
+			try :
+				tsl = [ele[1] for ele in tem]
+				val = [ele[0] for ele in tem]
+				for ind, ele in enumerate(tsl) :
+					if int(10*ele) >= 0 and int(10*ele) < len(time_slot(sensor)) :
+						tab[int(10*ele)] = val[ind]
+			except :
+				err.log('Failure for algorithmic extraction and sorting for sensor {}'.format(sensor))
 
-			if j >= 1 :
-				for k in range(j) :
-					values[len(values)-1-k] = values[len(values)-1-j]
+			def remplissage(values) :
 
-			# Apply linear regression to complete the missing data
-			for k in range(len(values)) :
-				if values[k] == 0 :
-					u = first(values[k:])
-					a = ((values[k+u]-values[k-1])/float((k+u-k+1)))
-					c = 0.5*(float(values[k+u]+values[k-1])-float(values[k+u]-values[k-1])*float(k+u+k-1)/float(k+u-k+1))
-					values[k] = float(int(10*(a*k+c))/10.0)
+				# Determine le premier indice non nul de la liste    
+				def first(liste) :
+					i = 0
+					l = liste
+					if l[0] != 0 : return(i)
+					else : 
+						while (l[0] == 0 and len(l) > 1) :
+							i += 1
+							l = l[1:]
+						return(i)
 
-			return(values)
+				# Determine le dernier indice non nul de la liste    
+				def last(liste) :
+					j = 0
+					l = liste
+					if l[-1] != 0 : return(j)
+					else :
+						while (l[-1] == 0 and len(l)>1) :
+							j += 1
+							l = l[:-1]
+						return(j)
 
-		try :
-			ful = remplissage(list(tab))
-			pwd = open('../Sample/Sam_{}_{}_{}.txt'.format(self.dte.strftime('%d-%m-%Y'), sensor[:1], sensor[1:]), 'w')
-			pwd.write(str(time_slot(sensor))+'\n')
-			pwd.write(str(ful)+'\n')
-			pwd.close()
-			msg.log('Sample created for sensor {}'.format(sensor))
-		except :
-			err.log('Could not create the sample for sensor {}'.format(sensor))
+				i, j = first(values), last(values)
 
-if __name__ == '__main__':
-	sam = Sampler(datetime.date.today())
-	sam.get_sample('TE')
+				if i >= 1 :
+					for k in range(i) : 
+						values[k] = values[i]
+
+				if j >= 1 :
+					for k in range(j) :
+						values[len(values)-1-k] = values[len(values)-1-j]
+
+				# Apply linear regression to complete the missing data
+				for k in range(len(values)) :
+					if values[k] == 0 :
+						u = first(values[k:])
+						a = ((values[k+u]-values[k-1])/float((k+u-k+1)))
+						c = 0.5*(float(values[k+u]+values[k-1])-float(values[k+u]-values[k-1])*float(k+u+k-1)/float(k+u-k+1))
+						values[k] = float(int(10*(a*k+c))/10.0)
+
+				return(values)
+
+			try :
+				ful = remplissage(list(tab))
+				pwd = open('../Sample/Sam_{}_{}_{}.txt'.format(self.dte.strftime('%d-%m-%Y'), sensor[:1], sensor[1:]), 'w')
+				pwd.write(str(time_slot(sensor))+'\n')
+				pwd.write(str(ful)+'\n')
+				pwd.close()
+				msg.log('Sample created for sensor {}'.format(sensor))
+			except :
+				err.log('Could not create the sample for sensor {}'.format(sensor))
+
+	def get_samples(self):
+
+		for gdr in ['T', 'H', 'L', 'M']:
+			for typ in [str(k) for k in range(1, 13)] + ['C', 'E'] :
+				self.get_sample(gdr + typ)
