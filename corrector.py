162e419890da19411251c219e5b6046d3dd4249e
Coricos <meryll.dindin@student.ecp.fr>
2017-03-05 16:39:29 +0100

    Put error and messenger classes as attributes of the corrector

--- corrector.py
+++ corrector.py
@@ -17,45 +17,59 @@ from error import *
 class Corrector:
 
 	def __init__(self):
+		self.err = Error()
+		self.msg = Messenger()
 		# First measurement on March the third
 		self.str = datetime.date(2016, 3, 1)
 		# Last day of correction
 		self.end = datetime.date.today()
 
-	def get_missing_qai(self):
+	def get_missing(self, db):
+
+		def remove_doublon(raw):
+			new = []
+			for val in raw :
+				if val not in new : new.append(val)
+			return new
 
 		mis = []
 
-		if not os.path.exists('../AirQuality/QAI_LCSQA') :
+		if not os.path.exists(db) :
 			for dte in rrule(DAILY, dtstart=self.str, until=self.end) :
 				mis.append(dte.date())
 
 			return missing
 
 		else :
-			raw = pd.read_pickle('../AirQuality/QAI_LCSQA') :
+			raw = pd.read_pickle(db) :
 			for dte in rrule(DAILY, dtstart=self.str, until=self.end) :
 				tem = dte.date()
-				if tem in raw.index() and np.isnan(raw[tem:tem].values[0][0]) :
+				idx = remove_doublon([ele.date() for ele in raw.index])
+				if tem in idx and np.isnan(raw[tem:tem].values[0][0]) :
 					mis.append(tem)
-				elif tem not in raw.index() :
+				elif tem not in idx :
 					mis.append(tem)
 
 			return missing
 
 	def correct_qai(self):
 
-		err = Error()
-		msg = Messenger()
-
-		mis = self.get_missing_qai()
-		msg.log('Air quality lacks {} days of collection'.format(len(mis)))
+		mis = self.get_missing('../AirQuality/QAI_LCSQA')
+		self.msg.log('Air quality lacks {} days of collection'.format(len(mis)))
 
 		for dte in mis :
 			qai = RequestQAI(dte)
 			qai.get_data()
 
-		msg.log('Air quality database successfully updated')
+		self.msg.log('Air quality database successfully updated')
 
-	def get_missing_wea(self):
+	def correct_wea(self):
 
+		mis = self.get_missing('../Weather/WEA')
+		self.msg.log('Weather lacks {} days of collection'.format(len(mis)))
+
+		for dte in mis :
+			wea = RequestWeather(dte)
+			wea.get_data()
+
+		self.msg.log('Weather database successfully updated')
