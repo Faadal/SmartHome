88925d712e28b2f017a1e7be8ce58baa2d0dd4ac
Coricos <meryll.dindin@student.ecp.fr>
2017-03-05 16:52:54 +0100

    New version with vision

--- schedule.py
+++ schedule.py
@@ -7,22 +7,30 @@ import datetime
 import time
 
 from api import *
+from database import *
+from corrector import *
 from processing import *
 
 # Get rid of the usual crontab
 
-api_eed = RequestEedomus()
-schedule.every().day.at('08:00').do(api_eed.get_data())
-schedule.every().day.at('20:00').do(api_eed.get_data())
+schedule.every().day.at('08:00').do(RequestEedomus().get_data())
 
-api_qai = RequestQAI()
-schedule.every().day.at('23:00').do(api_qai.get_data())
+schedule.every().day.at('20:00').do(RequestEedomus().get_data())
 
-api_wea = RequestWeather()
-schedule.every().day.at('23:15').do(api_wea.get_data())
+schedule.every().day.at('23:00').do(RequestQAI().get_data())
 
-sampler = Sample()
-schedule.every().day.at('23:30').do(sampler.get_samples())
+schedule.every().day.at('23:15').do(RequestWeather().get_data())
+
+schedule.every().day.at('23:30').do(Sampler().get_samples())
+
+# Schedule to correct databases once a week
+
+# Schedule to update database once a day
+
+# Schedule to clear what remains unnecessary once a week
+
+# Schedule for backup once a week (email ?)
+
 # Launch
 
 while True :
