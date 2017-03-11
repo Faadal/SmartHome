305c9eb2e48ea1b676e2c6dccfb3b448ca06f7c0
Coricos <meryll.dindin@student.ecp.fr>
2017-03-11 16:28:13 +0100

    Added last action for now - correction for database

--- scheduler.py
+++ scheduler.py
@@ -12,19 +12,19 @@ from processing import *
 
 # Get rid of the usual crontab
 
-schedule.every().day.at('08:00').do(RequestEedomus().get_data())
+schedule.every().day.at('08:00').do(RequestEedomus().get_data)
 
-schedule.every().day.at('20:00').do(RequestEedomus().get_data())
+schedule.every().day.at('20:00').do(RequestEedomus().get_data)
 
-schedule.every().day.at('23:15').do(RequestQAI().get_data())
+schedule.every().day.at('23:15').do(RequestQAI().get_data)
 
-schedule.every().day.at('23:30').do(RequestWeather().get_data())
+schedule.every().day.at('23:30').do(RequestWeather().get_data)
 
-schedule.every().day.at('00:15').do(Sampler().get_samples())
+schedule.every().day.at('00:15').do(Sampler().get_samples)
 
-schedule.every().day.at('00:30').do(Database('N227').update())
+schedule.every().day.at('00:30').do(Database('N227').update)
 
-schedule.every().monday.at('12:00').do(Corrector().correct_database('N227'))
+schedule.every().monday.at('12:00').do(Corrector('N227').correct_database)
 
 # Launch
 
