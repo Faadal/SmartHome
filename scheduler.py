81b3905a76ec03c01e7ade6f871fdc9c176c0de9
Coricos <meryll.dindin@student.ecp.fr>
2017-03-13 14:56:48 +0100

    Made sure that the parsed databases were corrected every day

--- scheduler.py
+++ scheduler.py
@@ -6,25 +6,31 @@ import schedule
 import time
 
 from api import *
+from tools import *
 from database import *
 from corrector import *
 from processing import *
 
 # Get rid of the usual crontab
 
+# Work on current day
 schedule.every().day.at('08:00').do(RequestEedomus().get_data)
-
+# Work on current day
 schedule.every().day.at('20:00').do(RequestEedomus().get_data)
-
-schedule.every().day.at('23:15').do(RequestQAI().get_data)
-
-schedule.every().day.at('23:30').do(RequestWeather().get_data)
-
-schedule.every().day.at('00:15').do(Sampler().get_samples)
-
-schedule.every().day.at('00:30').do(Database('N227').update)
-
-schedule.every().monday.at('12:00').do(Corrector('N227').correct_database)
+# Work on current day
+schedule.every().day.at('23:20').do(RequestWeather().get_data)
+# Work on current day
+schedule.every().day.at('23:30').do(RequestQAI().get_data)
+# Check missing dates and correct it
+schedule.every().day.at('05:00').do(Corrector('N227').correct_wea)
+# Check missing dates and correct it
+schedule.every().day.at('05:30').do(Corrector('N227').correct_qai)
+# Work on previous day
+schedule.every().day.at('06:00').do(Sampler().get_samples)
+# Work on previous day
+schedule.every().day.at('06:30').do(Database('N227').update)
+# Check missing dates and correct it
+schedule.every().day.at('12:00').do(Corrector('N227').correct_database)
 
 # Launch
 
