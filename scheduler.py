e575ce737f5079faa41172e4d7e542ec73315268
Coricos <meryll.dindin@student.ecp.fr>
2017-04-01 15:24:01 +0200

    New improvement due to new functionnalities

--- scheduler.py
+++ scheduler.py
@@ -2,8 +2,8 @@
 
 # Imports
 
-import schedule
 import time
+import schedule
 
 from api import *
 from tools import *
@@ -15,25 +15,32 @@ from processing import *
 
 # Work on current day
 schedule.every().day.at('08:00').do(RequestEedomus().get_data)
-# Work on current day
-schedule.every().day.at('20:00').do(RequestEedomus().get_data)
-# Work on current day
-schedule.every().day.at('23:20').do(RequestWeather().get_data)
-# Work on current day
-schedule.every().day.at('23:30').do(RequestQAI().get_data)
-# Check missing dates and correct it
-schedule.every().day.at('05:00').do(Corrector('N227').correct_wea)
-# Check missing dates and correct it
-schedule.every().day.at('05:30').do(Corrector('N227').correct_qai)
 # Work on previous day
 schedule.every().day.at('09:00').do(Sampler().get_samples)
 # Work on previous day
 schedule.every().day.at('09:30').do(Database('N227').update)
+# Work on previous day
+schedule.every().day.at('09:40').do(Database('E203').update)
+# Work on current day
+schedule.every().day.at('20:00').do(RequestEedomus().get_data)
+# Work on current day
+schedule.every().day.at('20:10').do(RequestFoobot().get_data)
+# Work on current day
+schedule.every().day.at('23:20').do(RequestWeather().get_data)
+# Work on current day
+schedule.every().day.at('23:30').do(RequestQAI().get_data)
+
 # Check missing dates and correct it
-schedule.every().day.at('12:00').do(Corrector('N227').correct_database)
+schedule.every().monday.at('05:00').do(Corrector('N227').correct_wea)
+# Check missing dates and correct it
+schedule.every().monday.at('05:30').do(Corrector('N227').correct_qai)
+# Check missing dates and correct it
+schedule.every().monday.at('12:00').do(Corrector('N227').correct_database)
+# Check missing dates and correct it
+schedule.every().monday.at('12:10').do(Corrector('E203').correct_database)
 
 # Launch
 
 while True :
 	schedule.run_pending()
-	time.sleep(1)
+	time.sleep(10)
