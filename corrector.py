c3a8301d43e077871f977270ab7a2eb7a372166f
Coricos <meryll.dindin@student.ecp.fr>
2017-04-14 15:12:32 +0200

    Ended version of sensors databases correction

--- corrector.py
+++ corrector.py
@@ -43,6 +43,7 @@ class Corrector:
 
 		else :
 			raw = pd.read_pickle(db)
+
 			for dte in rrule(DAILY, dtstart=self.str, until=self.end) :
 				tem = dte.date()
 				# Depending on the database, the indexes are date or datetime types - avoid conflicts and error types
@@ -110,38 +111,109 @@ class Corrector:
 				dbs.add_date_to_database(dte)
 		else :
 			dtf = pd.read_pickle(pwd)
+
+			# Take care of the duplicates
+			dtf = dtf.reset_index().drop_duplicates(subset='index', keep='last').set_index('index').sort_index()
+
 			for dte in tqdm.tqdm(ava) :
 				if dte not in dtf.index :
 					dbs.add_date_to_database(dte)
 
 	def correct_samples(self):
 
-		for ele in tqdm.tqdm(os.listdir('../Sample')) :
-
+		# Get the available dates
+		ava = {}
+		for ele in os.listdir('../Sample') :
 			try :
-				dte = parser.parse(ele[4:14])
-				typ = ele.split('-')[3][:4].split('_')[0]
-				sen = ele.split('-')[3][:4].split('_')[1]
-
-				pwd = '../Sensors/Sensor_{}_{}'.format(typ, sen)
-
-				raw = open('../Sample/' + ele, 'r')
-				tim, stl = raw.readline()[1:-2].replace(' ', '').split(','), []
-				for ele in tim[:-1] :
-					tem = datetime.datetime(year=dte.year, month=dte.month, day=dte.day, hour=int(float(ele)), minute=int((float(ele)-int(float(ele)))*60))
-					stl.append(tem)
-				val = raw.readline()[1:-2].replace(' ', '').split(',')
-				val = [float(e) for e in val][:-1]
-				raw.close()
-
-				if not os.path.exists(pwd) :
-					pd.DataFrame(data=np.asarray(val), index=np.asarray(stl), columns=np.asarray(['Value'])).to_pickle(pwd)
+				dte = parser.parse(ele[4:14], dayfirst=True).date()
+				typ = ele.split('-')[3].split('.')[0].split('_')[0]
+				sen = ele.split('-')[3].split('.')[0].split('_')[1]
+				if typ not in list(ava.keys()) : 
+					ava[typ] = {}
+					ava[typ][sen] = [dte]
+				elif sen not in list(ava[typ].keys()) :
+					ava[typ][sen] = [dte]
 				else :
-					dtf = pd.read_pickle(pwd)
-					if not dte.date() in [ele.date() for ele in dtf.index] :
-						new = pd.DataFrame(data=np.asarray(val), index=np.asarray(stl), columns=np.asarray(['Value']))
-						dtf = pd.concat([dtf, new])
-						dtf.to_pickle(pwd)
-
+					if dte not in ava[typ][sen] :
+						ava[typ][sen].append(dte)
 			except :
 				pass
+
+		print('|-> The list of available correction has been examined')
+
+		# Create the list of the databases we want to correct
+		lis = []
+		for typ in ['T', 'H', 'L'] :
+			for sen in [str(i) for i in range(1, 12)] :
+				lis.append(typ + sen)
+
+		for sen in tqdm.tqdm(lis) :
+
+			pwd = '../Sensors/Sensor_{}_{}'.format(sen[0], sen[1:])
+
+			if not os.path.exists(pwd) :
+
+				if sen[0] not in list(ava.keys()) : pass
+
+				elif sen[1:] not in list(ava[sen[0]].keys()) : pass
+
+				else :
+					dta, idx = [], []
+
+					for dte in ava[sen[0]][sen[1:]] :
+						raw = open('../Sample/Sam_{}-{}_{}.txt'.format(dte.strftime('%d-%m-%Y'), sen[0], sen[1:]), 'r')
+						tim, stl = raw.readline()[1:-2].replace(' ', '').split(','), []
+						for ele in tim[:-1] :
+							tem = datetime.datetime(year=dte.year, month=dte.month, day=dte.day, hour=int(float(ele)), minute=int((float(ele)-int(float(ele)))*60))
+							stl.append(tem)
+						val = raw.readline()[1:-2].replace(' ', '').split(',')
+						val = [float(e) for e in val][:-1]
+						raw.close()
+
+						for v in val : dta.append(v)
+						for s in stl : idx.append(s)
+
+					pd.DataFrame(data=np.asarray(dta), index=np.asarray(idx), columns=np.asarray(['Value'])).to_pickle(pwd)
+
+					self.msg.log('Database for sensor {} has been successfully created'.format(sen))
+			
+			else :
+
+				dtb = pd.read_pickle(pwd)
+				# Delete duplicates and sort the dataframe
+				dtb = dtb.reset_index().drop_duplicates(subset='index', keep='last').set_index('index').sort_index()
+
+				if sen[0] not in list(ava.keys()) : pass
+
+				elif sen[1:] not in list(ava[sen[0]].keys()) : pass
+				
+				else :
+
+					# Get the missing dates
+					dts, mis = remove_doublon([ele.date() for ele in dtb.index]), []
+					for dte in ava[sen[0]][sen[1:]] :
+						if dte not in dts : mis.append(dte)
+
+					self.msg.log('{} days of data were missing in the database'.format(len(mis)))
+
+					for dte in mis :
+
+						raw = open('../Sample/Sam_{}-{}_{}.txt'.format(dte.strftime('%d-%m-%Y'), sen[0], sen[1:]), 'r')
+						tim, stl = raw.readline()[1:-2].replace(' ', '').split(','), []
+						for ele in tim[:-1] :
+							tem = datetime.datetime(year=dte.year, month=dte.month, day=dte.day, hour=int(float(ele)), minute=int((float(ele)-int(float(ele)))*60))
+							stl.append(tem)
+						val = raw.readline()[1:-2].replace(' ', '').split(',')
+						val = [float(e) for e in val][:-1]
+						raw.close()
+
+						new = pd.DataFrame(data=np.asarray(val), index=np.asarray(stl), columns=np.asarray(['Value']))
+						dtb = pd.concat([dtb, new]).sort_index()
+						dtb.to_pickle(pwd)
+
+					self.msg.log('Database for sensor {} has been corrected'.format(sen))
+
+if __name__ == '__main__':
+	print(list(os.listdir('../Weather')))
+	#Corrector('').correct_wea()
+	Corrector('').correct_qai()
